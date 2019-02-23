cd8dbf01cf229579daf32b0146e647f54fcfc13c
Cole Hassett <cole@Coles-MacBook-Air.local>
2019-02-23 11:11:04 -0500

    style and index page

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ var express = require('express');
 var body_parser = require('body-parser');
 var app = express();
 
+var path = __dirname + '/views/';
+
 app.use(body_parser.urlencoded({extended: true}));
 app.use(body_parser.json());
 
 var routes = require('./api/routes/roastyRoute.js');
 routes(app);
+
+app.get('/', function(req, res){
+	res.render(path + "index.html");
+});
 
 var port = process.env.PORT || '8080';
 
